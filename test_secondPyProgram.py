80cff0035c61c9f59f4657c4d1ec4ab47f61816d
Sagar <sagarpant56@gmail.com>
2022-06-12 10:16:12 +0530

    Sincere Commit

--- test_secondPyProgram.py
+++ test_secondPyProgram.py
@@ -5,3 +5,9 @@ import pytest
 @pytest.mark.med
 def test_t1Module():
     print("Yo bro")
+
+
+@pytest.mark.usefixtures("conft")
+@pytest.mark.medical
+def test_t2Module():
+    print("Mere a new more Module!!!")
