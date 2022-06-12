e87ff164810645e375e9c6ea7710cb162b45568c
Sagar <sagarpant56@gmail.com>
2022-06-12 17:38:29 +0530

    Ultimate Commit!!!

--- test_firstPyProgram.py
+++ test_firstPyProgram.py
@@ -4,11 +4,14 @@ import pytest
 @pytest.mark.usefixtures("conft")
 class TestExample:
 
+
     def test_fixtureDemo(self):
         print("i will execute steps in fixtureDemo method")
 
+
     def test_fixtureDemo1(self):
         print("i will execute steps in fixtureDemo1 method")
+
 
     def test_fixtureDemo2(self):
         print("i will execute steps in fixtureDemo2 method")
